f34be2e5203e433306996f87b363e6addc5bced1
Eric Hong <f102041332@gmail.com>
2023-10-04 00:00:29 +0800

    [feature] map

--- src/components/SideBar/index.vue
+++ src/components/SideBar/index.vue
@@ -1,104 +1,96 @@
 <script setup>
-import { storeToRefs } from "pinia";
-import { useComponentStore } from "@/store/useComponentsStatusStore";
-import { ref } from "vue";
-import sidebar_bg from "@/assets/img/png/sidebar_bg.png";
+import { storeToRefs } from 'pinia';
+import { useComponentStore } from '@/store/useComponentsStatusStore';
+import { ref } from 'vue';
+import sidebar_bg from '@/assets/img/png/sidebar_bg.png';
 
-const { isShowSideBar, useful_tv, fontSizeController } = storeToRefs(
-	useComponentStore()
-);
+const { isShowSideBar, useful_tv, fontSizeController } = storeToRefs(useComponentStore());
 
-const defaultOpens = ref(["1", "1-1"]);
+const defaultOpens = ref(['1', '1-1']);
 </script>
 
 <template>
-	<div
-		:class="['side-bar', { 'side-bar-show': isShowSideBar }, 'side-bar-xl']"
-		:style="`background-image: url(${sidebar_bg})`"
-	>
-		<div class="logo"></div>
-		<div class="menu">
-			<el-menu
-				active-text-color="#ffd04b"
-				background-color="rgba(0,0,0,0)"
-				class="el-menu-vertical-demo"
-				:default-openeds="defaultOpens"
-				text-color="#fff"
-				:router="true"
-			>
-				<template v-for="(tv_1, t1) in useful_tv" :key="tv_1.name">
-					<el-sub-menu :index="`${t1 + 1}`" v-if="tv_1.path === ''">
-						<template #title>
-							<el-icon :size="fontSizeController + 2"><Watermelon /></el-icon>
-							<h1>{{ $t(tv_1.name) }}</h1>
-						</template>
-						<el-menu-item-group>
-							<template v-for="(tv_2, t2) in tv_1.children" :key="tv_2.name">
-								<el-sub-menu
-									:index="`${t1 + 1}-${t2 + 1}`"
-									v-if="tv_2.path === ''"
-								>
-									<template #title>
-										<el-icon :size="fontSizeController + 2">
-											<GobletSquareFull />
-										</el-icon>
-										<h1>{{ $t(tv_2.name) }}</h1>
-									</template>
-									<template v-for="tv_3 in tv_2.children" :key="tv_3.name">
-										<el-menu-item :index="tv_3.path" :disabled="false">
-											<el-icon :size="fontSizeController + 2"><Pear /></el-icon>
-											<h1>{{ $t(tv_3.name) }}</h1>
-										</el-menu-item>
-									</template>
-								</el-sub-menu>
-								<el-menu-item :index="tv_2.path" :disabled="false" v-else>
-									<el-icon :size="fontSizeController + 2"
-										><GobletSquareFull
-									/></el-icon>
-									<h1>{{ $t(tv_2.name) }}</h1>
-								</el-menu-item>
-							</template>
-						</el-menu-item-group>
-					</el-sub-menu>
-					<el-menu-item :index="tv_1.path" :disabled="false" v-else>
-						<el-icon :size="fontSizeController + 2"><Watermelon /></el-icon>
-						<h1>{{ $t(tv_1.name) }}</h1>
-					</el-menu-item>
-				</template>
-			</el-menu>
-		</div>
-	</div>
+  <div
+    :class="['side-bar', { 'side-bar-show': isShowSideBar }, 'side-bar-xl']"
+    :style="`background-image: url(${sidebar_bg})`"
+  >
+    <div class="menu">
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="rgba(0,0,0,0)"
+        class="el-menu-vertical-demo"
+        :default-openeds="defaultOpens"
+        text-color="#fff"
+        :router="true"
+      >
+        <template v-for="(tv_1, t1) in useful_tv" :key="tv_1.name">
+          <el-sub-menu :index="`${t1 + 1}`" v-if="tv_1.path === ''">
+            <template #title>
+              <el-icon :size="fontSizeController + 2"><Watermelon /></el-icon>
+              <h1>{{ $t(tv_1.name) }}</h1>
+            </template>
+            <el-menu-item-group>
+              <template v-for="(tv_2, t2) in tv_1.children" :key="tv_2.name">
+                <el-sub-menu :index="`${t1 + 1}-${t2 + 1}`" v-if="tv_2.path === ''">
+                  <template #title>
+                    <el-icon :size="fontSizeController + 2">
+                      <GobletSquareFull />
+                    </el-icon>
+                    <h1>{{ $t(tv_2.name) }}</h1>
+                  </template>
+                  <template v-for="tv_3 in tv_2.children" :key="tv_3.name">
+                    <el-menu-item :index="tv_3.path" :disabled="false">
+                      <el-icon :size="fontSizeController + 2"><Pear /></el-icon>
+                      <h1>{{ $t(tv_3.name) }}</h1>
+                    </el-menu-item>
+                  </template>
+                </el-sub-menu>
+                <el-menu-item :index="tv_2.path" :disabled="false" v-else>
+                  <el-icon :size="fontSizeController + 2"><GobletSquareFull /></el-icon>
+                  <h1>{{ $t(tv_2.name) }}</h1>
+                </el-menu-item>
+              </template>
+            </el-menu-item-group>
+          </el-sub-menu>
+          <el-menu-item :index="tv_1.path" :disabled="false" v-else>
+            <el-icon :size="fontSizeController + 2"><Watermelon /></el-icon>
+            <h1>{{ $t(tv_1.name) }}</h1>
+          </el-menu-item>
+        </template>
+      </el-menu>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .side-bar {
-	@apply m-0 h-full w-76 ease-linear duration-200 fixed -left-76  flex flex-col bg-no-repeat bg-bottom;
-	.logo {
-		@apply w-full h-32;
-	}
-	.menu {
-		@apply w-full h-full select-none overflow-y-auto overflow-x-hidden no-scrollbar;
-	}
+  @apply m-0 h-full w-76 ease-linear duration-200 fixed -left-76  flex flex-col bg-no-repeat bg-bottom bg-cover;
+  .logo {
+    @apply w-full h-32;
+  }
+  .menu {
+    @apply w-full h-full select-none overflow-y-auto overflow-x-hidden no-scrollbar;
+  }
 }
 .side-bar-xl {
-	@apply xl:-start-0.5 xl:relative;
+  @apply xl:-start-0.5 xl:relative;
 }
 
 .side-bar-show {
-	@apply -start-0.5;
+  @apply -start-0.5;
 }
 
 .el-menu {
-	border-right: none !important;
-	h1 {
-		@apply ml-3;
-	}
+  border-right: none !important;
+  h1 {
+    @apply ml-3;
+  }
 }
 
 .el-sub-menu:first-child {
-	@apply top-0;
+  @apply top-0;
 }
-.el-sub-menu [class^="el-icon-"] {
-	vertical-align: -1px !important;
+.el-sub-menu [class^='el-icon-'] {
+  vertical-align: -1px !important;
 }
 </style>
--- src/views/BackStage/index.vue
+++ src/views/BackStage/index.vue
@@ -1,110 +1,189 @@
 <script setup>
-import { Loader } from "@googlemaps/js-api-loader";
-import { reactive, onMounted, onBeforeMount, ref } from "vue";
+import { Loader } from '@googlemaps/js-api-loader';
+import { reactive, onMounted, onBeforeMount, ref } from 'vue';
 const states = reactive({
-	google: null,
-	map: null,
-	panorama: null,
+  google: null,
+  map: null,
+  panorama: null,
+  service: null,
+  directionsService: null,
+  directionsRender: null,
+  infoWindow: null,
 });
-const setXY = reactive({
-	x: 0,
-	y: 0,
+const currentLatLng = reactive({
+  lat: null,
+  lng: null,
 });
+
+const latlng = ref([
+  {
+    lat: 25.0298374,
+    lng: 121.5460298,
+    title: '新航',
+    heading: 240,
+    marker: null,
+  },
+  {
+    lat: 25.0316026,
+    lng: 121.5169051,
+    title: 'Card Master',
+    heading: 60,
+    marker: null,
+  },
+]);
+
 const isShow = ref(false);
 const initMap = async () => {
-	const loader = new Loader({
-		apiKey: "API-KEY",
-		version: "weekly",
-		libraries: ["places"],
-		language: "zh-TW",
-	});
-	states.google = await loader.load();
-	states.map = new states.google.maps.Map(document.getElementById("map"), {
-		center: { lat: 25.0425, lng: 121.5468 },
-		zoom: 11,
-		mapTypeControl: false,
-		fullscreenControl: false,
-		minZoom: 10,
-		maxZoom: 15,
-	});
-	states.map.addListener("zoom_changed", (event) => {
-		isShow.value = false;
-	});
-	states.map.addListener("click", (event) => {
-		states.panorama.setPosition(
-			new google.maps.LatLng({
-				lat: event.latLng.lat(),
-				lng: event.latLng.lng(),
-			})
-		);
-		states.map.setStreetView(states.panorama);
+  navigator.geolocation.getCurrentPosition(position => {
+    currentLatLng.lat = position.coords.latitude;
+    currentLatLng.lng = position.coords.longitude;
+  });
 
-		setTimeout(() => {
-			isShow.value = true;
-			setXY.x = event.domEvent.offsetX + 20;
-			setXY.y = event.domEvent.offsetY - 20;
-		}, 500);
-	});
-	states.panorama = new google.maps.StreetViewPanorama(
-		document.getElementById("pano"),
-		{
-			position: { lat: 25.0425, lng: 121.5468 },
-			pov: {
-				heading: 34,
-				pitch: 10,
-			},
-		}
-	);
-	states.panorama.addListener("position_changed", () => {
-		console.log(states.panorama.getPosition().lat());
-		console.log(states.panorama.getPosition().lng());
-		states.map.setCenter({
-			lat: states.panorama.getPosition().lat(),
-			lng: states.panorama.getPosition().lng(),
-		});
-	});
-	states.map.setStreetView(states.panorama);
+  const loader = new Loader({
+    apiKey: 'AIzaSyBs1F_wf3P9yNe38Sucld1THY8vEuq7FZk',
+    version: 'weekly',
+    libraries: ['places'],
+    language: 'zh-TW',
+  });
+  states.google = await loader.load();
+
+  states.map = new states.google.maps.Map(document.getElementById('map'), {
+    center: { lat: currentLatLng.lat, lng: currentLatLng.lng },
+    zoom: 11,
+    mapTypeControl: false,
+    fullscreenControl: false,
+    minZoom: 10,
+    maxZoom: 15,
+  });
+  states.panorama = new states.google.maps.StreetViewPanorama(document.getElementById('pano'), {
+    position: { lat: currentLatLng.lat, lng: currentLatLng.lng },
+    pov: {
+      heading: 34,
+      pitch: 10,
+    },
+  });
+  states.service = new states.google.maps.places.PlacesService(states.map);
+  states.directionsService = new states.google.maps.DirectionsService();
+  states.infoWindow = new states.google.maps.InfoWindow();
+
+  latlng.value.forEach((ll, i) => {
+    latlng.value[i].marker = new states.google.maps.Marker({
+      position: { lat: ll.lat, lng: ll.lng },
+      map: states.map,
+      title: ll.title,
+    });
+  });
+
+  latlng.value.forEach(ll => {
+    const { marker, ...other } = ll;
+    marker.addListener('click', event => {
+      const markerLL = new states.google.maps.LatLng(other.lat, other.lng);
+      states.service.nearbySearch(
+        { location: markerLL, keyword: marker.title, radius: 20 },
+        (res, status) => {
+          if (status === 'OK' && res.length === 1) {
+            const station = res[0];
+            console.log(station);
+            states.panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), {
+              position: {
+                lat: other.lat,
+                lng: other.lng,
+              },
+              pov: {
+                heading: other.heading,
+                pitch: 10,
+              },
+            });
+
+            if (!states.directionsRender)
+              states.directionsRender = new states.google.maps.DirectionsRenderer({
+                map: states.map,
+              });
+
+            states.directionsRender.set('directions', null);
+
+            states.directionsService.route(
+              {
+                origin: new states.google.maps.LatLng(currentLatLng.lat, currentLatLng.lng),
+                destination: {
+                  placeId: station.place_id,
+                },
+                travelMode: states.google.maps.TravelMode['TRANSIT'],
+              },
+              (rout, status) => {
+                console.log(rout, status);
+                if (status === 'OK') {
+                  states.directionsRender.setDirections(rout);
+                  states.infoWindow.setContent(
+                    `
+										<h3>Name :${station.name}</h3>
+										<div>Address :${station.vicinity}</div>
+										<div>Open Now :${station.opening_hours.isOpen() ? 'OPEN' : 'CLOSE'}</div>
+										<div>Directions :${rout.routes[0].legs[0].duration.text}</div>
+										`,
+                  );
+                  states.infoWindow.open({
+                    anchor: marker,
+                    map: states.map,
+                  });
+                }
+              },
+            );
+          }
+        },
+      );
+    });
+  });
+
+  states.map.setStreetView(states.panorama);
+  // event
+  states.map.addListener('zoom_changed', event => {
+    isShow.value = false;
+  });
+  states.map.addListener('click', event => {
+    states.panorama.setPosition(
+      new google.maps.LatLng({
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      }),
+    );
+  });
+
+  states.map.addListener('drag', () => {});
+
+  states.panorama.addListener('position_changed', () => {
+    states.map.setCenter({
+      lat: states.panorama.getPosition().lat(),
+      lng: states.panorama.getPosition().lng(),
+    });
+  });
+  states.panorama.addListener('drag', event => {
+    console.log(event);
+  });
 };
 
 onBeforeMount(async () => {
-	await initMap();
+  await initMap();
 });
 </script>
 
 <template>
-	<div class="map">
-		<div id="map"></div>
-		<div id="pano"></div>
-		<div
-			:class="['pop', { show: isShow }]"
-			:style="{
-				left: `${setXY.x}px`,
-				top: `${setXY.y}px`,
-			}"
-		></div>
-	</div>
+  <div class="map">
+    <div id="map"></div>
+    <div id="pano"></div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .map {
-	width: 100%;
-	height: 100%;
-	position: relative;
-	#map,
-	#pano {
-		float: left;
-		height: 100%;
-		width: 50%;
-	}
-	.pop {
-		display: none;
-		position: fixed;
-		width: 200px;
-		height: 200px;
-		background-color: #fff;
-	}
-	.show {
-		display: block;
-	}
+  width: 100%;
+  height: 100%;
+  position: relative;
+  #map,
+  #pano {
+    float: left;
+    height: 100%;
+    width: 50%;
+  }
 }
 </style>
